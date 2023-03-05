0bd8ece64027c3b994f7bbcf15edf1e8e9ca2d2f
Andrew <github@acheremisov.com>
2023-03-05 11:39:44 -0500

    Comments and fixes

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ const axios = require("axios");
 const express = require("express");
 
 const app = express();
-const currency = process.env.CURRENCY;
-const voipMsApiUrl = `https://voip.ms/api/v1/rest.php?content_type=json&api_username=${api_username}&api_password=${api_password}&method=${method}`;
 
 const getBalance = (api_username, api_password) => {
   const method = "getBalance";
+  const voipMsApiUrl = `https://voip.ms/api/v1/rest.php?content_type=json&api_username=${api_username}&api_password=${api_password}&method=${method}`;
   return new Promise((resolve, reject) => {
     axios
       .get(voipMsApiUrl)
@@ -41,19 +40,29 @@ const getBalance = (api_username, api_password) => {
 
 app.get("/", async function (req, res, next) {
   const userAgent = req.header("user-agent");
+
   // Adding additional security for Groundwire app only
   if (userAgent.includes("Groundwire/")) {
+    // Defining variables
+    const currency = process.env.CURRENCY;
     const api_username = encodeURIComponent(process.env.USERNAME);
     const api_password = process.env.PASSWORD;
+
+    // Requesting balance
     const balance = await getBalance(api_username, api_password);
+
+    // Building JSON for Groundwire
     const objBalance = {
       balanceString: currency + " " + balance,
       balance: balance,
       currency,
     };
+
+    // Outputing request to Docker logs and sending it back
     console.log(userAgent);
     res.json(objBalance);
   } else {
+    // If not Groundwire - send 404
     res.status(404).end();
   }
 });
